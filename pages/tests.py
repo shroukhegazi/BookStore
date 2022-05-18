54ff4e682679ce97bbbad89a0e50aceb90af3ff8
shroukhegazi <shroukhegazi99@gmail.com>
2022-05-18 17:32:07 +0200

    created pages app,templates, urls, views and their tests

---
+++ pages/tests.py
@@ -0,0 +1,36 @@
+
+from urllib import response
+from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from pages.urls import HomePageView 
+
+
+class HomepageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+
+    def test_home_page_template(self):
+        self.assertTemplateUsed(self.response,'home.html')    
+
+    def test_homepage_contains_correct_html(self): 
+        self.assertContains(self.response, 'Homepage') 
+
+
+    def test_homepage_does_not_contain_incorrect_html(self): 
+        self.assertNotContains(self.response, 'what is wrong')      
+
+
+    def test_homepage_url_resolves_homepageview(self): 
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+            )    
